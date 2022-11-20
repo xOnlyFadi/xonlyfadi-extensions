e818ce105086dcdfdb7761ab931fe28d056785bd
xOnlyFadi <63213308+xOnlyFadi@users.noreply.github.com>
2022-11-20 08:23:20 +0000

    Deploying to gh-pages from @ xOnlyFadi/xonlyfadi-extensions@df3dd7e966f99281b4be43e6528721474e8d77b0 🚀

--- 0.6/DesuME/source.js
+++ 0.6/DesuME/source.js
@@ -6650,7 +6650,7 @@ const DOMAIN = 'https://desu.me';
 const API = `${DOMAIN}/manga/api`;
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36';
 exports.DesuMEInfo = {
-    version: '1.0.0',
+    version: '1.0.1',
     name: 'Desu',
     icon: 'icon.png',
     author: 'xOnlyFadi',
@@ -6915,7 +6915,7 @@ class DesuME extends paperback_extensions_common_1.Source {
     }
     CloudFlareError(status) {
         if (status == 503) {
-            throw new Error('CLOUDFLARE BYPASS ERROR:\nPlease go to Settings > Sources > GManga and press Cloudflare Bypass or press the Cloud image on the right');
+            throw new Error('CLOUDFLARE BYPASS ERROR:\nPlease go to Settings > Sources > Desu and press Cloudflare Bypass or press the Cloud image on the right');
         }
     }
 }
